6f7da195dfa2172fe263579bed2561076a4dcf4d
Tony_Shuck <tony.shuck25@gmail.com>
2026-04-14 16:41:51 -0700

    app.jsx changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState("Connecting to Backend...")
 
   useEffect(() => {
-    fetch(`${import.meta.env.VITE_API_URL}/api/test/`)
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/api/test/`);
       .then(response => {
         if (!response.ok) throw new Error("Network response was not ok");
         return response.json();
